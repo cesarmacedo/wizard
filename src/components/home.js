0b058e8b73809a2a4e7e408516eac40abbf42731
vitorDaynno <vitordaynno@gmail.com>
2020-04-05 20:29:12 -0300

    Removing test code

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import {View} from 'react-native';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      user: 'a',
-      token: 'aa',
-    };
-  }
-
-  componentDidMount() {
-    this.getToken();
-  }
-
-  async getToken() {
-    const user = await AsyncStorage.getItem('@user');
-    const token = await AsyncStorage.getItem('@token');
-    this.setState({user, token});
   }
 
   render() {
-    return (
-      <View>
-        <Text>{this.state.user}</Text>
-        <Text>{this.state.token}</Text>
-      </View>
-    );
+    return <View />;
   }
 }
